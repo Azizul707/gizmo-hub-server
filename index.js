a1a72acaa916341c38e1630b450e7fadac8cbe87
Azizul <arafinazizul@gmail.com>
2023-10-23 03:26:46 +0600

    add to cart product posted

--- index.js
+++ index.js
@@ -38,41 +38,49 @@ async function run() {
   try {
       await client.connect();
 
-      const productsCollection = client.db( 'productsDB' ).collection( 'product' );
+    const productsCollection = client.db( 'productsDB' ).collection( 'product' );
+    const addToCartCollection = client.db('productsDB').collection('addToCart')
+    
+    // app.get("/products/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await productsCollection.findOne(query);
+    //   res.send(result);
+    // });
       
-      app.post( '/products', async( req, res ) => {
-          const newProduct = req.body;
-          console.log( newProduct );
-          const result = await productsCollection.insertOne( newProduct );
-          res.send( result );
-      } )
+    app.post( '/products', async ( req, res ) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne( newProduct );
+      res.send( result );
+    } );
+
+    app.post( '/cart', async ( req, res ) => {
+      const product = req.body
+      console.log( product );
+      const result = await addToCartCollection.insertOne( product );
+      res.send( result );
+    } );
     
     app.get( '/products', async ( req, res ) => {
       const cursor = productsCollection.find();
       const result = await cursor.toArray();
       res.send( result );
-    })
+    } );
+    
+    app.get( '/cart', async ( req, res ) => {
+      const cursor = addToCartCollection.find();
+      const result = await cursor.toArray();
+      res.send( result );
+    } );
 
 
     app.get( '/products/:brand', async ( req, res ) => {
       const brand = req.params.brand;
-
       const query = { brandname: brand };
-
-      
-      const cursor = productsCollection.find(query);
-      const result = await cursor.toArray((err, products) => {
-        if (err) {
-          console.error(err);
-          e
-        } else {
-          // Products matching the BrandName are in the "products" array
-          // console.log(products);
-          // Handle the retrieved products, e.g., return them as a response
-        }
-      });
+      const cursor = productsCollection.find( query );
+      const result = await cursor.toArray();
       res.send( result );
-    })
+    } );
 
     await client.db("admin").command({ ping: 1 });
     console.log(
